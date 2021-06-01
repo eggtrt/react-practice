52cfbc1079460c91b3f8a64fb0f95e4b24d806dc
jisu <eggtrt@gmail.com>
2021-06-01 23:56:01 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import Hello from './hello';
 import Wrapper from './Wrapper';
 
+function countActiveUsers (users) {
+  console.log('count users...');
+  return users.filter(user => user.active).length
+}
 function App() {
+  const [user, setUser] = useState();
+  const count = useMemo(countActiveUsers(users), [user]);
   return (
     <>
     <Hello name="react" color="red"></Hello>
