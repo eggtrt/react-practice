a222fcc519df7d27d516fd292aa047723c3eed05
jisu <eggtrt@gmail.com>
2021-06-01 23:51:06 +0900

    wrapper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Hello from './hello';
+import Wrapper from './Wrapper';
 
 function App() {
   return (
+    <>
     <Hello name="react" color="red"></Hello>
+    <Wrapper></Wrapper>
+    </>
   );
 }
 
---
+++ src/Wrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Wrapper ({ children }) {
+    return (
+        <div style={style}>{children}</div>
+    )
+}
+
+export default Wrapper;
