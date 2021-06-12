351e8022ef1d75c3c2c0a1f6d3d05a62fe1cf34e
eggtrt <eggtrt@gmail.com>
2021-06-12 20:37:25 +0900

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { useMemo, useState } from 'react';
-import Hello from './hello';
-import InputSample from './InputSample';
-import Wrapper from './Wrapper';
+import React, { useRef } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
-function countActiveUsers (users) {
-  console.log('count users...');
-  return users.filter(user => user.active).length
-}
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
   return (
     <>
-    <InputSample></InputSample>
-    <Wrapper></Wrapper>
+      <CreateUser />
+      <UserList users={users} />
     </>
   );
 }
