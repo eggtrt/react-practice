791ce78b5745853312552aa436276195c8072ff1
eggtrt <eggtrt@gmail.com>
2021-06-19 12:25:00 +0900

    class counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,24 @@
-import React, { useReducer } from 'react';
+import React, { Component } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
+class Counter extends Component {
+  handleIncrease() {
+    console.log('increase');
+    console.log(this);
+  }
+
+  handleDecrease() {
+    console.log('decrease');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>0</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+      </div>
+    );
   }
 }
 
-function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    dispatch({ type: 'INCREMENT' });
-  };
-
-  const onDecrease = () => {
-    dispatch({ type: 'DECREMENT' });
-  };
-
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
-}
-
-export default React.memo(Counter);
+export default Counter;
