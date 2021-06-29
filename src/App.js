8b8ef92f8d42ece1d716a030647fed91a0b8e7b9
eggtrt <eggtrt@gmail.com>
2021-06-29 23:59:38 +0900

    context API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -8,10 +9,6 @@ function countActiveUsers(users) {
 }
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: ''
-  },
   users: [
     {
       id: 1,
@@ -36,17 +33,8 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE_INPUT':
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
     case 'CREATE_USER':
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user)
       };
     case 'TOGGLE_USER':
@@ -66,21 +54,18 @@ function reducer(state, action) {
   }
 }
 
+// UserDispatch 라는 이름으로 내보내줍니다.
+export const UserDispatch = React.createContext(null);
+
 function App() {
+  const [{ username, email }, onChange, onReset] = useInputs({
+    username: '',
+    email: ''
+  });
   const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
 
   const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback(e => {
-    const { name, value } = e.target;
-    dispatch({
-      type: 'CHANGE_INPUT',
-      name,
-      value
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
@@ -91,8 +76,9 @@ function App() {
         email
       }
     });
+    onReset();
     nextId.current += 1;
-  }, [username, email]);
+  }, [username, email, onReset]);
 
   const onToggle = useCallback(id => {
     dispatch({
@@ -108,8 +94,9 @@ function App() {
     });
   }, []);
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
-    <>
+    <UserDispatch.Provider value={dispatch}>
       <CreateUser
         username={username}
         email={email}
@@ -117,8 +104,8 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
-      <div>활성사용자 수 : 0</div>
-    </>
+      <div>활성사용자 수 : {count}</div>
+    </UserDispatch.Provider>
   );
 }
 
