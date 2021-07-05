d9070452efca6324195c66c0b3e59edaf7126a73
eggtrt <eggtrt@gmail.com>
2021-07-05 22:35:53 +0900

    rest  props3

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,11 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-function Button({ children, size, color, outline, fullWidth, onClick, onMouseMove }) {
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
   return (
     <button
       className={classNames('Button', size, color, { outline, fullWidth })}
-      onClick={onClick}
-      onMouseMove={onMouseMove}
+      {...rest}
     >
       {children}
     </button>
