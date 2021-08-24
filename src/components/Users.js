51a1e55feb156dabfaedec481757ac2c1df43795
eggtrt <eggtrt@gmail.com>
2021-08-24 14:51:00 +0900

    userasync

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,43 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
-import User from './User';
+import { useAsync } from 'react-async';
 
-// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
-// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
-async function getUsers() {
+async function getUser({ id }) {
   const response = await axios.get(
-    'https://jsonplaceholder.typicode.com/users'
+    `https://jsonplaceholder.typicode.com/users/${id}`
   );
   return response.data;
 }
 
-function Users() {
-  const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(getUsers, [], true);
+function User({ id }) {
+  const { data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id
+  });
 
-  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
-
-  if (loading) return <div>로딩중..</div>;
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!user) return null;
+
   return (
-    <>
-      <ul>
-        {users.map(user => (
-          <li
-            key={user.id}
-            onClick={() => setUserId(user.id)}
-            style={{ cursor: 'pointer' }}
-          >
-            {user.username} ({user.name})
-          </li>
-        ))}
-      </ul>
-      <button onClick={refetch}>다시 불러오기</button>
-      {userId && <User id={userId} />}
-    </>
+    <div>
+      <h2>{user.username}</h2>
+      <p>
+        <b>Email:</b> {user.email}
+      </p>
+    </div>
   );
 }
 
-export default Users;
+export default User;
