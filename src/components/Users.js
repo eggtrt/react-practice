d8e3d9ea86cb81b2f68ba571e2b53d330b1eaa0a
eggtrt <eggtrt@gmail.com>
2021-08-25 14:51:00 +0900

    run

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useAsync } from 'react-async';
+import User from './User';
 
-async function getUser({ id }) {
+async function getUsers() {
   const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
+    'https://jsonplaceholder.typicode.com/users'
   );
   return response.data;
 }
 
-function User({ id }) {
-  const { data: user, error, isLoading } = useAsync({
-    promiseFn: getUser,
-    id,
-    watch: id
+function Users() {
+  const [userId, setUserId] = useState(null);
+  const { data: users, error, isLoading, run } = useAsync({
+    deferFn: getUsers
   });
 
   if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!user) return null;
+  if (!users) return <button onClick={run}>불러오기</button>;
 
   return (
-    <div>
-      <h2>{user.username}</h2>
-      <p>
-        <b>Email:</b> {user.email}
-      </p>
-    </div>
+    <>
+      <ul>
+        {users.map(user => (
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={run}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </>
   );
 }
 
-export default User;
+export default Users;
