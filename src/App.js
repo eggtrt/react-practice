a78eaa9e5c79c9bd3ecda8d207de45fa13dea249
eggtrt <eggtrt@gmail.com>
2021-09-02 14:51:00 +0900

    link router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
-import TodoTemplate from './components/TodoTemplate';
-import TodoHead from './components/TodoHead';
-import TodoList from './components/TodoList';
-import TodoCreate from './components/TodoCreate';
-import { TodoProvider } from './TodoContext';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    background: #e9ecef;
-  }
-`;
-
-function App() {
+const App = () => {
   return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </div>
   );
-}
+};
 
 export default App;
