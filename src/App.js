1480ca76632cccee09a53c0bd5103cfe7da48cb2
eggtrt <eggtrt@gmail.com>
2021-09-21 14:51:00 +0900

    postList

--- src/App.js
+++ src/App.js
@@ -1,45 +1,15 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import About from './About';
-import Home from './Home';
-import Profiles from './Profiles';
-import HistorySample from './HistorySample';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필 목록</Link>
-        </li>
-        <li>
-          <Link to="/history">예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다:</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
+    <>
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
+    </>
   );
-};
+}
 
 export default App;
